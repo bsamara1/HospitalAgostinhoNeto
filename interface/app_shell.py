e9a6236e1a848cac6801fed778ab85ca5388b395
bsamara1 <btavares.l23@us.edu.cv>
2026-06-30 19:10:52 -0100

    dfgh

--- interface/app_shell.py
+++ interface/app_shell.py
@@ -6,49 +6,31 @@ class AppShell(ctk.CTkToplevel):
     def __init__(self, master):
         super().__init__(master)
 
-        self.title("HAN - Hospital Agostinho Neto")
+        self.title("HAN")
         self.after(100, lambda: self.state("zoomed"))
 
         self.configure(fg_color="#F5F7FB")
 
+        # 🔥 container fixo
+        self.container = ctk.CTkFrame(self)
+        self.container.pack(fill="both", expand=True)
+
         self.show_login()
 
-    # ==================================================
-    def _clear(self):
-        for w in self.winfo_children():
+    def clear(self):
+        for w in self.container.winfo_children():
             w.destroy()
 
-    # ==================================================
     def show_login(self):
-        self._clear()
+        self.clear()
 
         from interface.login import LoginContent
 
-        LoginContent(
-            self,
-            on_success=self.show_app,
-            on_register=self.show_register   # 🔥 IMPORTANTE
-        )
+        LoginContent(self.container, self.show_app, self.show_register)
 
-    # ==================================================
-    def show_register(self):
-        self._clear()
-
-        from interface.criar_conta import CriarContaContent
-
-        CriarContaContent(
-            self,
-            on_back=self.show_login
-        )
-
-    # ==================================================
     def show_app(self, sessao):
-        self._clear()
+        self.clear()
 
         from interface.main_window import MainContent
 
-        MainContent(
-            self,
-            on_logout=self.show_login,
-            sessao=sessao
-        )
+        MainContent(self.container, sessao)
