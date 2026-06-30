e9d9d263fa473b8e9b4141cc03ecd8d858633a49
Mileida Lopes <mlopes.l23@us.edu.cv>
2026-06-30 23:39:58 +0100

    commit de pacientes dash

---
+++ interface/pacientes_dash/__init__.py
@@ -0,0 +1,13 @@
+﻿from .dashboard import DashboardPacienteContent
+from .minhas_consultas import MinhasConsultasContent
+from .notificacoes import NotificacoesContent
+from .reagendar import ReagendarContent
+from .cancelar import CancelarContent
+
+__all__ = [
+    "DashboardPacienteContent",
+    "MinhasConsultasContent",
+    "NotificacoesContent",
+    "ReagendarContent",
+    "CancelarContent",
+]
